8d583ed29518bbbcaece60d68508818dbc844ef2
tglueckGIT <48133650+tglueckGIT@users.noreply.github.com>
2019-05-19 14:11:17 +0200

    Add files via upload

--- assignment1.py
+++ assignment1.py
@@ -1,22 +1,26 @@
 import mysql.connector
+import os
+import pysam
+import pybedtools
 
-__author__ = 'XXX'
-
-##
-## Concept:
-## TODO
-##
-
+__author__ = 'Glueck Tobias'
 
 class Assignment1:
     
     def __init__(self):
         ## Your gene of interest
-        self.gene = ""
+        self.gene = "CLDN14"
+        self.geneList = []
+        self.lineList = []
+        self.genedict = self.download_gene_coordinates("hg38", "fetched_genes")
+        self.bamfile = os.path.join(os.getcwd(), "chr21.bam")
+        self.baifile = os.path.join(os.getcwd(), "chr21.bam.bai")
+        self.samfile = pysam.AlignmentFile(self.bamfile, "rb")
+        self.reads = list(self.samfile.fetch("chr21", 1, 100))#self.genedict["txStart"], self.genedict["txEnd"]))
 
-    
     def download_gene_coordinates(self, genome_reference, file_name):
         ## TODO concept
+        self.file = file_name
         
         print("Connecting to UCSC to fetch data")
         
@@ -44,13 +48,21 @@ class Assignment1:
         cursor.execute(query)
         
         ## Write to file
-        ## TODO this may need some work 
-        with open(file_name, "w") as fh:
+        ## TODO this may need some work
+        #geneList = []
+        #lineList = []
+        line = 0
+        with open("output.txt", "w") as fh:
             for row in cursor:
+            	line = line + 1
                 fh.write(str(row) + "\n")
-    
-            
+                if (row[0] == self.gene):
+                	self.lineList.append(line)
+                	self.geneList.append(row) ## all lines relevant to the gene are in this list now
         ## Close cursor & connection
+        # QNAME    FLAG   RNAME        POS     MAPQ CIGAR  
+        #CLDN14 NM_012130 chr21 36460620,36466441 - 2 36460620,36466366 36461776,36466441
+        #print(self.geneList)
         cursor.close()
         cnx.close()
         
@@ -58,46 +70,109 @@ class Assignment1:
         
     def get_coordinates_of_gene(self):
         ## Use UCSC file
-        print("todo")
+        strCoordinates = str(self.geneList[0][3]) + " - " + str(self.geneList[0][4])
+        return(strCoordinates)
         
     def get_gene_symbol(self):
-        print("todo")
+        return(self.geneList[0][0])
                         
     def get_sam_header(self):
-        print("todo")
+        self.alignfile = pysam.AlignmentFile(self.file, "rb")
+        strHeader = ""
+        for i, j in self.alignfile.header["HD"].items():
+            if i == "SO":
+                strHeader += "\tSO (Sorting order of Alignments): " + str(j)
+            if i == "VN":
+                strHeader += "\tVN (Format version): " + str(j)
+            if i == "GO":
+                strHeader += "\tGO: (Grouping of alignments): " + str(j)
+        return(strHeader)
         
     def get_properly_paired_reads_of_gene(self):
-        print("todo")
+        strPaired = ""
+        i = 0
+        for read in self.reads:
+            if read.is_proper_pair:
+                i += 1
+        if i==0:
+            strPaired = "No properly paired read"
+        else:
+            strPaired = "Number of properly paired reads: " + str(i)
+        return(strPaired)
         
     def get_gene_reads_with_indels(self):
-        print("todo")
+        strIndels = ""
+        i=0
+        for read in self.reads:
+            if not read.is_unmapped:
+                cigar = read.cigar
+                for (type, length) in cigar:
+                    if (type ==1) or (type == 2):
+                        i+=1
+        if i == 0:
+            strIndels = "No gene reads with indels"
+        else:
+            strIndels= "Number of gene reads with indels: " + str(i)
+        return(strIndels)
         
     def calculate_total_average_coverage(self):
-        print("todo")
+        bam = pybedtools.BedTool(self.bamfile)
+        cov = bam.genome_coverage(bg=True, genome="hg38")
+        i = 0
+        sum = 0
+        for line in cov:
+            number = float(line[3])
+            sum += number
+            i+=1
+        return(sum/i)
         
     def calculate_gene_average_coverage(self):
-        print("todo")
+        a = pybedtools.BedTool(self.bamfile)
+        b = a.genome_coverage(bg=True)
+        sum = 0
+        i=0
+
+        for line in b:
+            number = float(line[3])
+            beg = int(line[1])
+            if beg > self.genedict["tx:Start"]:
+                if int(line[2]) <= self.genedict["txEnd"]:
+                    sum += number
+                    i+=1
+        return(sum/i)
         
     def get_number_mapped_reads(self):
-        print("todo")
+        i=0
+        for read in self.reads:
+            if not read.is_unmapped:
+                i+=1
+        return(i)
 
     def get_region_of_gene(self):
-        print("todo")
+        return(self.geneList[0][2])
         
     def get_number_of_exons(self):
-        print("ads")
+        return(self.geneList[0][6])
     
     
     def print_summary(self):
-        print("Print all results here")
+        print("Coordinateds of gene: " + str(self.get_coordinates_of_gene()))
+        print("Gene symbol: " + str(self.get_gene_symbol()))
+        print("Sam Header: " + str(self.get_sam_header()))
+        print("Properly paired reads: " + str(self.get_properly_paired_reads_of_gene()))
+        print("Reads with indels: " + str(self.get_gene_reads_with_indels()))
+        print("Total average coverage: " + str(self.calculate_total_average_coverage()))
+        print("Gene average coverage: " + str(self.calculate_gene_average_coverage()))
+        print("Number of mapped reads: " + str(self.get_number_mapped_reads()))
+        print("Region of gene: " + str(self.get_region_of_gene()))
+        print("Number of exons: " + str(self.get_number_of_exons()))
     
     
 def main():
     print("Assignment 1")
     assignment1 = Assignment1()
+    assignment1.download_gene_coordinates("hg38", "chr21.bam")
     assignment1.print_summary()
-    
-    
     print("Done with assignment 1")
     
         
